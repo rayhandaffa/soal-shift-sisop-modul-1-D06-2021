27a2d081aa10cca24517b30d1394b9709a0896f7
Ramadhan Arif H <dani.ramadhan7@gmail.com>
2021-04-02 14:31:02 +0700

    Add files 3c and 3d

---
+++ soal3/soal3d.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#soal3d.sh
+
+image=1
+
+Divisor=2
+Day=$(date -d "$D" '+%d')
+let Mod=$Day%$Divisor
+
+if [ $Mod -eq 1 ]
+then
+    # Kucing_
+    Date=$(date +'%d-%m-%Y')
+    mkdir "Kucing_$Date"
+    cd "Kucing_$Date"
+else
+    # Keclinci_
+    Date=$(date +'%d-%m-%Y')
+    mkdir "Kelinci_$Date"
+    cd "Kelinci_$Date"
+fi
+
+for((i=1;image<=23;i=i+1))
+do
+    duplicate=0
+
+    wget "https://loremflickr.com/320/240/kitten" -a Foto.log -O newKitten
+
+    if [ $i -eq 1 ]
+    then
+        mv newKitten `printf "Koleksi_%02d" "$image"`
+        image=$(($image+1))
+    fi
+
+    if [ $i -ne 1 ]
+    then
+        for ((j=1;j<image;j=j+1))
+        do
+            prevKitten=`printf "Koleksi_%02d" "$j"`
+
+            #check for duplicates with comparison
+            reduplicate=`cmp $prevKitten newKitten -b`
+
+            if [ -z "$reduplicate" ]
+            then
+                duplicate=1
+                break
+            else
+                duplicate=0
+            fi
+        done  
+    fi
+
+    if [ $i -gt 1 ]
+    then
+        if [ $duplicate -eq 1 ]
+        then
+            rm newKitten
+        else
+            mv newKitten `printf "Koleksi_%02d" "$image"`
+            image=$((image+1))
+        fi
+    fi
+done
+
+cd -
+
+if [ $Mod -eq 1 ]
+then
+    # Kucing_
+    DatePass=$(date +'%d%m%Y')
+    zip -P "$DatePass" -r Koleksi.zip "Kucing_$Date"
+else
+    # Keclinci_
+    DatePass=$(date +'%d%m%Y')
+    zip -P "$DatePass" -r Koleksi.zip "Kelinci_$Date"
+fi
